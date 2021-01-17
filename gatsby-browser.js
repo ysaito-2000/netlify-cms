802bb73749310f2051d6906022443d5815f0b933
Yuki Saito <saitoyukisaito@gmail.com>
2021-01-16 17:10:57 -0800

    Add files via upload

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+import CustomLayout from "./wrapPageElement"
+import "./static/styles/fonts.css"
+import "./static/styles/normalize.css"
+// import Amplify, { Auth } from "aws-amplify"
+// import awsConfig from "./src/aws-exports"
+// Amplify.configure(awsConfig)
+
+export const wrapPageElement = CustomLayout
