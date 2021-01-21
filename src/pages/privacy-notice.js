5eed5d88c1085ceca09c8deda74e50efe7206e92
yuki_s <saitoyukisaito@gmail.com>
2021-01-21 00:58:50 -0800

    footer pages share one component

--- src/pages/privacy-notice.js
+++ src/pages/privacy-notice.js
@@ -1,41 +1,14 @@
 import React from "react"
-import { graphql } from "gatsby"
 import SEO from "../components/SEO/SEO"
+import FooterPage from "../components/FooterPage/FooterPage"
 
-function PrivacyNotice(props) {
-    const data = props.data.allMarkdownRemark.edges[0];
+function PrivacyNotice() {
     return (
         <>
             <SEO title="Terms of Use" />
-            <div className="legal__body">
-                <div className="legal__header">
-                    {data ? data.node.frontmatter.title : null}
-                </div>
-                <div className="legal__date">
-                    {data ? data.node.frontmatter.date : null}
-                </div>
-                <div className="legal_info">
-                    {data ? data.node.rawMarkdownBody : null}
-                </div>
-            </div>
+            <FooterPage file="privacy" />
         </>
     )
 }
 
 export default PrivacyNotice
-
-export const query = graphql`
-query {
-    allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(privacy)/"}}) {
-        edges {
-          node {
-            frontmatter {
-              date
-              title
-            }
-            rawMarkdownBody
-          }
-        }
-      }
-}
-`
