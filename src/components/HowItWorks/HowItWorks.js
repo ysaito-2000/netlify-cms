d0132d1c2fd74c4ede8ee74e65a5a75801179b85
Jonathan <jon@jonathanlau.io>
2021-01-21 17:36:15 -0800

    license information apache

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -12,18 +12,13 @@ function HowItWorks() {
       <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
         <div className="sm:text-center lg:text-left">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block xl:inline">
-              Our mission & our responsibility to humanity: {" "}
-            </span>
-
+            <span className="block xl:inline">adapt to you</span>
+            Let our products adapt to you
             <span className="block text-indigo-600 xl:inline">
-              No shady privacy policies or back doors for advertisers.
+              Customize and update our products to seamlessly fit your life –
+              all within your browser.
             </span>
           </h1>
-          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-            Our mission & our responsibility to humanity: No shady privacy
-            policies or back doors for advertisers.
-          </p>
           <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
             <div className="rounded-md shadow">
               <a
