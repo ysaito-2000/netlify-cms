0fb6bc3290df5f1b2a738cb5320b261ed07e7fff
Yuki Saito <saitoyukisaito@gmail.com>
2021-01-21 21:40:47 -0800

    commented out aws

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,8 +2,8 @@ import "./src/styles/global.css"
 import CustomLayout from "./wrapPageElement"
 import "./static/styles/fonts.css"
 import "./static/styles/normalize.css"
-import Amplify, { Auth } from "aws-amplify"
-import awsConfig from "./src/aws-exports"
-Amplify.configure(awsConfig)
+// import Amplify, { Auth } from "aws-amplify"
+// import awsConfig from "./src/aws-exports"
+// Amplify.configure(awsConfig)
 
 export const wrapPageElement = CustomLayout
